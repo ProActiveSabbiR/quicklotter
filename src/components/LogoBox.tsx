80c11650d490069ea90c71d8aaee6915653b7a8b
ProActiveSabbiR <webdvsabbr@gmail.com>
2025-08-01 21:13:42 +0600

    Update main logo to use quick_lotter_logo.svg

--- src/components/LogoBox.tsx
+++ src/components/LogoBox.tsx
@@ -1,6 +1,5 @@
 import { NavbarBrand } from 'react-bootstrap'
-import logoLight from '@/assets/images/logo-light.svg'
-import logo from '@/assets/images/logo.svg'
+import quickLotterLogo from '@/assets/images/quick_lotter_logo.svg'
 import logoSmall from '@/assets/images/logo-icon.svg'
 import Image from 'next/image'
 import Link from 'next/link'
@@ -16,8 +15,7 @@ const LogoBox = ({ className, smallIcon, imageClassName }: LogoBoxProps) => {
     <Image src={logoSmall} className={className ?? ''} alt="logo"  />
   ) : (
     <NavbarBrand as={Link} className={className ?? ''} href="">
-      <Image className={`light-mode-item h-40px ${imageClassName ?? ''} `} src={logo} alt="logo" width={119} height={40} />
-      <Image className={`dark-mode-item h-40px ${imageClassName ?? ''}`} src={logoLight} alt="logo" width={119} height={40} />
+      <Image className={`h-40px ${imageClassName ?? ''} `} src={quickLotterLogo} alt="Quick Lotter Logo" width={119} height={40} />
     </NavbarBrand>
   )
 }
